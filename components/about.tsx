9799238d650f8ca84de902f0e54a3b97c2687ffd
BANGATA <atanasiusditya@gmail.com>
2023-01-07 14:09:43 +0700

    fix: remove atropos

--- components/about.tsx
+++ components/about.tsx
@@ -9,8 +9,6 @@ import {
 } from "@chakra-ui/react";
 import Navbar from "./navbar";
 import { motion } from "framer-motion";
-import Atropos from "atropos/react";
-import "atropos/css";
 
 const About = () => {
   return (
@@ -21,198 +19,196 @@ const About = () => {
         bgRepeat={"no-repeat"}
         bgImage="./space.gif"
         bgPosition="center"
+        //color={"white"}
       >
         <Navbar />
-        <Flex h={"91.5vh"} justify={"center"} align={"center"}>
-          <Atropos activeOffset={40} shadowScale={1.05}>
+        <Flex h={"91.5vh"} justify={"center"} align={"center"} color={"white"}>
+          <Flex
+            border={"solid"}
+            h={[600, 550, 500, 500]}
+            w={[350, 500, 700, 1100]}
+            backdropFilter="auto"
+            backdropBlur="8px"
+            borderRadius={25}
+            boxShadow={"dark-lg"}
+            justifyContent={"center"}
+            alignItems={"center"}
+            flexDir={["column", "column", "row", "row"]}
+            color={"white"}
+          >
             <Flex
-              border={"solid"}
-              h={[600, 550, 500, 500]}
-              w={[350, 500, 700, 1100]}
-              backdropFilter="auto"
-              backdropBlur="8px"
+              justify={"space-between"}
+              align={"center"}
+              h={"100%"}
+              w={"100%"}
               borderRadius={25}
-              boxShadow={"dark-lg"}
-              justifyContent={"center"}
-              alignItems={"center"}
               flexDir={["column", "column", "row", "row"]}
             >
               <Flex
-                justify={"space-between"}
-                align={"center"}
+                //border={"solid"}
+                w={["100%", "100%", "50%", "50%"]}
                 h={"100%"}
-                w={"100%"}
-                borderRadius={25}
-                flexDir={["column", "column", "row", "row"]}
+                justifyContent={"center"}
+                alignItems={"center"}
+                borderLeftRadius={25}
               >
-                <Flex
-                  //border={"solid"}
-                  w={["100%", "100%", "50%", "50%"]}
-                  h={"100%"}
+                <Stack
                   justifyContent={"center"}
                   alignItems={"center"}
-                  borderLeftRadius={25}
+                  spacing={3}
+                  //border={"solid"}
+                  h={"100%"}
+                  w={"100%"}
                 >
-                  <Stack
-                    justifyContent={"center"}
-                    alignItems={"center"}
-                    spacing={3}
-                    //border={"solid"}
-                    h={"100%"}
-                    w={"100%"}
+                  <Avatar
+                    src={"foto.webp"}
+                    w={[75, 75, 150, 150]}
+                    h={[75, 75, 150, 150]}
+                    m={3}
+                  />
+                  <Text
+                    fontFamily={"Nabla"}
+                    fontSize={[14, 20, 28, 32]}
+                    textAlign={"center"}
                   >
-                    <Avatar
-                      src={"foto.webp"}
-                      w={[75, 75, 150, 150]}
-                      h={[75, 75, 150, 150]}
-                      m={3}
-                    />
-                    <Text
-                      fontFamily={"Nabla"}
-                      fontSize={[14, 20, 28, 32]}
-                      textAlign={"center"}
-                    >
-                      Raditya Herkristito
-                    </Text>
-                    <Text
-                      fontFamily={"Mina"}
-                      fontSize={[12, 14, 16, 18]}
-                      fontWeight={"bold"}
-                    >
-                      Frontend Developer
-                    </Text>
-                    <Text
-                      textAlign={"justify"}
-                      px={5}
-                      fontFamily={"Mina"}
-                      fontSize={[12, 12, 12, 16]}
-                    >
-                      I am an undergraduate student majoring in computer science
-                      at Universitas Multimedia Nusantara and an passionate
-                      Frontend Developer. Interested in 3D Website, Creative
-                      Coding, and Frontend Development. Presently learning to
-                      develop my skills in 3D website development. Equipped with
-                      a diverse and promising skill-set. Able to effectively
-                      self-manage during independent projects, as well as
-                      collaborate in a team setting.
-                    </Text>
-                  </Stack>
-                </Flex>
-                <Flex
-                  //border={"solid"}
-                  w={["100%", "100%", "50%", "50%"]}
-                  h={"100%"}
+                    Raditya Herkristito
+                  </Text>
+                  <Text
+                    fontFamily={"Mina"}
+                    fontSize={[12, 14, 16, 18]}
+                    fontWeight={"bold"}
+                    color={"white"}
+                  >
+                    Frontend Developer
+                  </Text>
+                  <Text
+                    textAlign={"justify"}
+                    px={5}
+                    fontFamily={"Mina"}
+                    fontSize={[12, 12, 12, 16]}
+                    color={"white"}
+                  >
+                    I am an undergraduate student majoring in computer science
+                    at Universitas Multimedia Nusantara and an passionate
+                    Frontend Developer. Interested in 3D Website, Creative
+                    Coding, and Frontend Development. Presently learning to
+                    develop my skills in 3D website development. Equipped with a
+                    diverse and promising skill-set. Able to effectively
+                    self-manage during independent projects, as well as
+                    collaborate in a team setting.
+                  </Text>
+                </Stack>
+              </Flex>
+              <Flex
+                //border={"solid"}
+                w={["100%", "100%", "50%", "50%"]}
+                h={"100%"}
+                justifyContent={"center"}
+                alignItems={"center"}
+                borderRightRadius={25}
+                mb={3}
+              >
+                <Stack
+                  w={"full"}
                   justifyContent={"center"}
                   alignItems={"center"}
-                  borderRightRadius={25}
-                  mb={3}
+                  spacing={5}
                 >
-                  <Stack
+                  <Text fontFamily={"Nabla"} fontSize={[14, 20, 28, 32]}>
+                    Language
+                  </Text>
+                  <HStack
+                    //border={"solid"}
                     w={"full"}
-                    justifyContent={"center"}
-                    alignItems={"center"}
+                    justify={"center"}
+                    align={"center"}
                     spacing={5}
                   >
-                    <Text fontFamily={"Nabla"} fontSize={[14, 20, 28, 32]}>
-                      Language
-                    </Text>
-                    <HStack
-                      //border={"solid"}
-                      w={"full"}
-                      justify={"center"}
-                      align={"center"}
-                      spacing={5}
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
                     >
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"html.png"}
-                          alt="html"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image src={"css.png"} alt="css" w={[50, 50, 50, 75]} />
-                      </motion.div>
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"js.png"}
-                          alt="javascript"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"ts.png"}
-                          alt="typescript"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                    </HStack>
-                    <Text fontFamily={"Nabla"} fontSize={[14, 20, 28, 32]}>
-                      Framework
-                    </Text>
-                    <HStack
-                      //border={"solid"}
-                      w={"full"}
-                      justify={"center"}
-                      align={"center"}
-                      spacing={5}
+                      <Image src={"html.png"} alt="html" w={[50, 50, 50, 75]} />
+                    </motion.div>
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
                     >
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"next.png"}
-                          alt="Next.js"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"react.png"}
-                          alt="react"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                      <motion.div
-                        whileHover={{
-                          scale: 1.2,
-                        }}
-                      >
-                        <Image
-                          src={"django.png"}
-                          alt="django"
-                          w={[50, 50, 50, 75]}
-                        />
-                      </motion.div>
-                    </HStack>
-                  </Stack>
-                </Flex>
+                      <Image src={"css.png"} alt="css" w={[50, 50, 50, 75]} />
+                    </motion.div>
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
+                    >
+                      <Image
+                        src={"js.png"}
+                        alt="javascript"
+                        w={[50, 50, 50, 75]}
+                      />
+                    </motion.div>
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
+                    >
+                      <Image
+                        src={"ts.png"}
+                        alt="typescript"
+                        w={[50, 50, 50, 75]}
+                      />
+                    </motion.div>
+                  </HStack>
+                  <Text fontFamily={"Nabla"} fontSize={[14, 20, 28, 32]}>
+                    Framework
+                  </Text>
+                  <HStack
+                    //border={"solid"}
+                    w={"full"}
+                    justify={"center"}
+                    align={"center"}
+                    spacing={5}
+                  >
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
+                    >
+                      <Image
+                        src={"next.png"}
+                        alt="Next.js"
+                        w={[50, 50, 50, 75]}
+                      />
+                    </motion.div>
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
+                    >
+                      <Image
+                        src={"react.png"}
+                        alt="react"
+                        w={[50, 50, 50, 75]}
+                      />
+                    </motion.div>
+                    <motion.div
+                      whileHover={{
+                        scale: 1.2,
+                      }}
+                    >
+                      <Image
+                        src={"django.png"}
+                        alt="django"
+                        w={[50, 50, 50, 75]}
+                      />
+                    </motion.div>
+                  </HStack>
+                </Stack>
               </Flex>
             </Flex>
-          </Atropos>
+          </Flex>
         </Flex>
       </Flex>
     </>
